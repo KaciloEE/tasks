5829852fa2e5dd4a20292c8bcb289234a82bb9d5
KaciloEE <KaciloEE@gmail.com>
2016-10-27 23:34:24 +0300

    test

---
+++ Kata/test.py
@@ -0,0 +1,12 @@
+a = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+size=int(len(a))
+
+s1 = []
+s2 = []
+for i in range(len(a)):
+    s1.append(a[i][i])
+
+for i in range(len(a)):
+    s2.append(a[i][(size-1)-i])
+
+print(abs(sum(s1)-sum(s2)))
